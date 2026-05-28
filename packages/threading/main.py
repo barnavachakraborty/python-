9555314f19ec74f8ee9ad74e8df10e80e631c984
barnavachakraborty <barnavachakraborty987@gmail.com>
2026-05-28 08:52:11 +0530

    threading

--- packages/threading/main.py
+++ packages/threading/main.py
@@ -3,18 +3,20 @@ import time
 
 start = time.perf_counter()
 
-def do_something():
-    print("sleeping for 5 secs")
-    time.sleep(5)
+def do_something(seconds):
+    print(f"sleeping for {seconds} secs")
+    time.sleep(seconds)
     print("done Sleeping")
 
-t1 = threading.Thread(target = do_something)
-t2 = threading.Thread(target = do_something)
+threads = []
 
-t1.start()
-t2.start()
-t1.join()
-t2.join()
+for  _  in range(10):
+    t = threading.Thread(target=do_something, args=[1])
+    t.start()
+    threads.append(t)
+
+for thread in threads:
+    thread.join()
 
 end = time.perf_counter()
 
