16eefce0c836fdb398a322a1ca067ec6aa85ad80
barnavachakraborty <barnavachakraborty987@gmail.com>
2026-07-20 20:46:32 +0530

    made it private

--- image processing/4_RFCM/13_features.py
+++ image processing/4_RFCM/13_features.py
@@ -1,7 +1,7 @@
 #1. paths -> typehint: str | os.PathLike[str]
 #2. to get the image pixels as pixel -> img = Image.open(filename).convert("L")
 
-
+from numba import njit
 from PIL import Image
 import logging
 import numpy as np
@@ -23,23 +23,23 @@ class features:
         if not os.path.isfile(filename):
             raise FileNotFoundError(f"'{filename}' does not exist")
         try:
-            img = Image.open(filename).convert("L")
+            _img = Image.open(filename).convert("L")
         except Exception as e:
             raise ValueError(f"Could not read Image '{filename}'.") from e
-        self.img: npt.NDArray[np.uint8] = np.array(img)
-        self.height = img.height
-        self.width = img.width
+        self.img: npt.NDArray[np.uint8] = np.array(_img)
+        self.height = _img.height
+        self.width = _img.width
         
     @property
     def features(self):
-        glcm_dtype = ([
+        _glcm_dtype = ([
             ('0_deg',np.float32),
             ('45_deg',np.float32),
             ('90_deg',np.float32),
             ('135_deg',np.float32)
         ])
-        pixel_dtype = np.dtype([
-            ('GLCM',glcm_dtype),
+        _pixel_dtype = np.dtype([
+            ('GLCM',_glcm_dtype),
             ('homogeneity',np.float32),
             ('edge',np.float32),
             ('asm',np.float32),
@@ -53,36 +53,30 @@ class features:
             ('diff_var',np.float32),
             ('diff_entropy',np.float32),
         ])
-        features = np.zeros(self.img.shape,dtype=pixel_dtype)
-        homogeneity = features['homogeneity']
-        edge = features['edge']
-        asm = features['asm']
-        contrast = features['contrast']
-        correlation = features['correlation']
-        idm = features['idm']
-        sum_avg = features['sum_avg']
-        sum_var = features['sum_var']
-        sum_entropy = features['sum_entropy']
-        entropy = features['entropy']
-        diff_var = features['diff_var']
-        diff_entropy = features['diff_entropy']
-        GLCM = features['GLCM']
-        
-                
-                
-        
+        _features = np.zeros(self.img.shape,dtype=_pixel_dtype)
+        _homogeneity = _features['homogeneity']
+        _edge = _features['edge']
+        _asm = _features['asm']
+        _contrast = _features['contrast']
+        _correlation = _features['correlation']
+        _idm = _features['idm']
+        _sum_avg = _features['sum_avg']
+        _sum_var = _features['sum_var']
+        _sum_entropy = _features['sum_entropy']
+        _entropy = _features['entropy']
+        _diff_var = _features['diff_var']
+        _diff_entropy = _features['diff_entropy']
+        _GLCM = _features['GLCM']
+    
+    
         
         
         
-        
-        
-        
-        
-
 
 if __name__ == '__main__':
     img1 = features(r'E:\python\image processing\4_RFCM/Images\1_090.pgm')
     print(img1.img.shape[0])
     print(img1.img[20,100])
+    
     
         
